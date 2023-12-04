d3a444c6079b02a7e25ac520363eee5fc94e66f0
Itohan Ukponmwan <itohanukponmwan@gmail.com>
2023-12-03 18:28:12 -0800

    update settings

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "try_on.apps.TryOnConfig",
     "storages",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -110,6 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
